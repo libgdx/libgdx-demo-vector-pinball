10125642836b7f502d21348454b7b12e3bb8b821
TomSki <tomaszwojciechowski@hotmail.co.uk>
2014-04-09 16:18:10 +0100

    Regen with new setup

---
+++ android/src/com/dozingcatsoftware/bouncy/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dozingcatsoftware.bouncy.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dozingcatsoftware.bouncy.Bouncy;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Bouncy(), config);
+	}
+}
---
+++ desktop/src/com/dozingcatsoftware/bouncy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dozingcatsoftware.bouncy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dozingcatsoftware.bouncy.Bouncy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bouncy(), config);
+	}
+}
